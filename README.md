8608538e9593213804e5f0a1d3efc664affe8f7b
fbpalmeira <fbarbosapalmeira@nerdwallet.com>
2024-08-15 15:49:15 -0300

    Adding all necessary files for deploying this Python app

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # filterpods
 Repository for the filterpods app
+The lastest image used by the Python app is stored in the Docker Hub registry
+https://hub.docker.com/r/ltwistz/filterpods
+
+The python script was deployed to a docker image through a Dockerfile and then the image was pushed to the registry.
+Then I created the necessary resources so the pod executing this code in a K3D (fastest one I could think of) so it could have access to cluster info. 
+Then created a helm chart to deploy it easier.
+
+Hope you enjoy it.
---
+++ filterpods.py
@@ -0,0 +1,27 @@
+from kubernetes import client, config
+from kubernetes.client.rest import ApiException
+import os
+import sys
+
+phase = os.environ['POD_STATUS']
+
+def list_pods_by_status_state(phase):
+    config.load_incluster_config()
+    v1 = client.CoreV1Api()
+
+    try:
+        print(f"Listing all pods with status: {phase}")
+        all_pods = v1.list_pod_for_all_namespaces(watch=False)
+        print("%s\t%s\t%s" % ("Pod name", "Pod namespace", "Pod Status"))
+        for pod in all_pods.items:
+            pod_phase=pod.status.phase
+            if phase in pod_phase:
+                print("%s\t%s\t%s" % (pod.metadata.name, pod.metadata.namespace, pod.status.phase))
+
+    except ApiException as e:
+        print(f"Exception when calling CoreV1Api->list_pod_for_all_namespaces: {e}\n")
+        sys.exit(1)
+
+    
+if __name__ == "__main__":
+    list_pods_by_status_state(phase)
